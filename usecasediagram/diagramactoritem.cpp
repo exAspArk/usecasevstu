7ed8a8f893579a2aa2830f2e9675088a9aabcb2b
Nickolay <Nickolay@localhost>
2011-10-25 15:45:58 +0400

    ???????? ??????????? ???????? "?????????"

--- usecasediagram/diagramactoritem.cpp
+++ usecasediagram/diagramactoritem.cpp
@@ -19,7 +19,29 @@ QVariant DiagramActorItem::itemChange(GraphicsItemChange change,
 }
 void DiagramActorItem::focusOutEvent(QFocusEvent *event)
 {
+    QString str;
     setTextInteractionFlags(Qt::NoTextInteraction);
+    str=this->toPlainText();
+    int i=0;
+    int pos=0;
+    bool imgFlag=false;
+    while(i<str.size()&&imgFlag!=true)
+    {
+        if(str.at(i).unicode()>256)
+        {
+            imgFlag=true;
+            pos=i;
+        }
+        i++;
+    }
+    if(imgFlag==false)
+    {
+        this->setHtml(QString("<img src=\":/images/actor.png\" />")+"<p>"+str+"</p>");
+    }
+    if(imgFlag==true&&(pos-1)!=0)
+    {
+        this->setHtml(QString("<img src=\":/images/actor.png\" />")+"<p>"+str.right(str.size()-2-pos+1)+"</p>");
+    }
     emit lostFocus(this);
     QGraphicsTextItem::focusOutEvent(event);
 }
